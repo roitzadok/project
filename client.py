3ce75a87a2553f75fa57f80a6d45763af68418ec
Roi Tzadok <roitzadok1@walla.com>
2017-04-04 14:17:43 +0300

    final database

--- client.py
+++ client.py
@@ -1,15 +1,20 @@
 import socket
 import numpy
+import platform
 
 
+MY_PLATFORM = platform.system()
 BUFF_SIZE = numpy.getbufsize()
 
+
 def main():
+    print MY_PLATFORM
     client_socket = socket.socket()
     server_add = ('127.0.0.1', 20)
     client_socket.connect(server_add)
-    client_socket.send(raw_input("enter whatever you want to send to the server "))
-    data=client_socket.recv(BUFF_SIZE)
+    data_to_send=raw_input("enter whatever you want to send to the server ")
+    client_socket.send(data_to_send)
+    data = client_socket.recv(BUFF_SIZE)
     print data
 
 
--- database.py
+++ database.py
@@ -2,34 +2,46 @@ import sqlite3
 import os
 
 
+DATABASE_FILE="database.db"
+
 class DataBase:
-    def get_url(self,download_name):
+    def get_url(self, download_name):
         print download_name
         return "somthing"
+
     def __init__(self):
-        if not os.path.exists("database.db"):
-            conn = sqlite3.connect('database.db')
+        if not os.path.exists(DATABASE_FILE):
+            conn = sqlite3.connect(DATABASE_FILE)
             cursor = conn.cursor()
             # create table
             cursor.execute("""
-            CREATE TABLE downloads_websites
-                        (download_name TEXT PRIMARY KEY, website_name TEXT)
+            CREATE TABLE downloads_websites_urls
+                        (download_name TEXT PRIMARY KEY, website_name TEXT,url TEXT)
             """)
-            cursor.execute("INSERT INTO downloads_websites "
-                           "VALUES ('python','https://www.python.org')")
+            cursor.execute("INSERT INTO downloads_websites_urls "
+                           "VALUES ('python','https://www.python.org',"
+                           "'https://www.python.org/ftp/python/2.7.13/python-2.7.13.msi,"
+                           "https://www.python.org/ftp/python/2.7.13/python-2.7.13-macosx10.6.pkg')")
             # create table
             cursor.execute("""
-            CREATE TABLE websites_
-                        (download_name TEXT PRIMARY KEY, website_name TEXT)
+            CREATE TABLE urls_devices_rating
+                        (url TEXT PRIMARY KEY, device_or_devices TEXT, rating_number_of_people)
             """)
+            cursor.execute("INSERT INTO urls_devices_rating "
+                           "VALUES ('https://www.python.org/ftp/python/2.7.13/python-2.7.13.msi','windows','5,1')")
+            cursor.execute("INSERT INTO urls_devices_rating "
+                           "VALUES ('https://www.python.org/ftp/python/2.7.13/python-2.7.13-macosx10.6.pkg'"
+                           ",'windows','5,1')")
         else:
-            conn = sqlite3.connect('database.db')
+            conn = sqlite3.connect(DATABASE_FILE)
             cursor = conn.cursor()
         conn.commit()
-        print_table(cursor,"downloads_websites")
+        print_table(cursor, "downloads_websites_urls")
+        print_table(cursor,"urls_devices_rating")
         conn.close()
-        os.remove("database.db")# remove me later!!!!!!!!!!!!!!!!!!!
+        os.remove(DATABASE_FILE)  # remove me later!!!!!!!!!!!!!!!!!!!
 
-def print_table(cursor,table_name):
-    cursor.execute("SELECT * FROM %s"%(table_name))
+
+def print_table(cursor, table_name):
+    cursor.execute("SELECT * FROM %s" % (table_name))
     print(cursor.fetchall())
